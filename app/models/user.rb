a85d55d831db8190b8edb45707d7fb3f3c521c26
jooshka <jooshka@mail.ru>
2017-01-19 17:57:48 +0300

    add devise mail confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { avatar: "219x200>", icon: "75x75>" }, default_url: "/images/missing.png"
+  has_attached_file :avatar, styles: { avatar: "219x200>", icon: "75x75>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :pets
